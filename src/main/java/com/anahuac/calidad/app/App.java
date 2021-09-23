6c529d78dfb285781271009eecdd21dc3a9b18b2
Jose <00371464@anahuacmayab.edu.>
2021-09-23 18:46:44 -0500

    Modificacion desde ecli

--- src/main/java/com/anahuac/calidad/app/App.java
+++ src/main/java/com/anahuac/calidad/app/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola mundo" );
+        System.out.println( "modificacioon eclipse" );
+        System.out.println( "modifica\sdsdslipse" );
+        System.out.println( "modifi" );
+        System.out.println( "modifi" );
     }
 }
